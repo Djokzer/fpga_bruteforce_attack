16e63efd7a160b4d2e40c6a891efcf590a74e0b5
Djokzer <abivarmank@gmail.com>
2024-07-17 16:14:43 +0200

    tx packet status done without pwd support, need to test it with testbench and add pwd support

--- hardware/uart_bcrypt/hdl/packet/tx_packet_status_ctrl.vhd
+++ hardware/uart_bcrypt/hdl/packet/tx_packet_status_ctrl.vhd
@@ -1,3 +1,10 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.math_real.all;
+
+library work;
+
 entity tx_packet_status_ctrl is
     generic (
         NUMBER_OF_QUADCORES : integer := 1
@@ -18,11 +25,11 @@ entity tx_packet_status_ctrl is
         dout                : in std_logic_vector (31 downto 0);
 
         -- TX PIPELINE INTERFACE
-        status_ready        : in std_logic;
+        status_ready        : out std_logic;
         status_clear        : in std_logic;
-        pwd_ready           : in std_logic;
-        pwd_clear           : in std_logic
-        data                : out std_logic_vector(7 downto 0);
+        pwd_ready           : out std_logic;
+        pwd_clear           : in std_logic;
+        data                : out std_logic_vector(7 downto 0)
     );
 end entity tx_packet_status_ctrl;
 
@@ -36,6 +43,43 @@ architecture rtl of tx_packet_status_ctrl is
     signal quadcore_count_load  : std_logic := '0'; 
     signal quadcore_count_in    : integer := 0;
 
+    -- STATE MACHINE STATUS
+    type states_stat_t is (
+        S_RESET,
+        RESTART,
+        FILL_BUFFER,
+        READY_TO_SEND, WAIT_FOR_CLEAR,
+        SEND_DATA
+    );
+    signal current_state_stat : states_stat_t := S_RESET;
+    signal next_state_stat    : states_stat_t := S_RESET;
+
+    -- STATE MACHINE PWD
+    type states_pwd_t is (
+        S_RESET,
+        WAIT_FOR_SUCCESS,
+        GET_PASSWORD, 
+        READY_TO_SEND, WAIT_FOR_CLEAR,
+        SEND_DATA
+    );
+    signal current_state_pwd : states_pwd_t := S_RESET;
+    signal next_state_pwd    : states_pwd_t := S_RESET;
+
+    -- CONTROL SIGNALS
+    signal status_rts : std_logic := '0';
+    signal select_status : std_logic := '0';
+    signal pwd_rts : std_logic := '0';
+    signal select_pwd : std_logic := '0';
+
+    -- COUNTERS SIGNALS FOR DATA OUT
+    signal data_0_count       : integer := 0;
+    signal data_0_count_en    : std_logic := '0'; 
+    signal data_0_count_load  : std_logic := '0'; 
+    signal data_0_count_in    : integer := 0;
+    signal data_1_count       : integer := 0;
+    signal data_1_count_en    : std_logic := '0'; 
+    signal data_1_count_load  : std_logic := '0'; 
+    signal data_1_count_in    : integer := 0;
 begin
 
 -- TO DO :
@@ -43,7 +87,7 @@ begin
 --  WHEN BUFFER IS FULL, SEND A PACKET WITH ALL FETCHED COUNT
 --  WHEN PASSWORD HAS BEEN FOUND, SHOULD SIGNAL IT AND SEND A PACKET WITH THE PASSWORD IN
 
-    -- COUNT TO ADDRESS EACH QUADCORE
+    -- COUNT TO GET EACH QUADCORE
     quadcore_counter : process(clk)
     begin
         if rising_edge(clk) then
@@ -72,7 +116,145 @@ begin
         end if;
     end process;
 
-    -- FSM / 
+    -- DATA OUT COUNTER 0
+    data_out_counter_0 : process(clk)
+    begin
+        if rising_edge(clk) then
+            if reset = '1' then
+                data_0_count <= 0;
+            else
+                if data_0_count_load = '1' then
+                    data_0_count <= data_0_count_in;
+                elsif data_0_count_en = '1' then
+                    data_0_count <= data_0_count + 1;
+                end if;
+            end if;
+        end if;
+    end process;
 
+    -- DATA OUT COUNTER 1
+    data_out_counter_1 : process(clk)
+    begin
+        if rising_edge(clk) then
+            if reset = '1' then
+                data_1_count <= 0;
+            else
+                if data_1_count_load = '1' then
+                    data_1_count <= data_1_count_in;
+                elsif data_1_count_en = '1' then
+                    data_1_count <= data_1_count + 1;
+                end if;
+            end if;
+        end if;
+    end process;
 
+    -- FSM STATUS
+    -- FSM UPDATE
+    fsm_state_stat : process(clk)
+    begin
+        if rising_edge(clk) then
+            if reset = '1' then
+                current_state_stat <= S_RESET;
+            else
+                current_state_stat <= next_state_stat;
+            end if; -- rst
+        end if; -- clk
+    end process;
+
+    -- FSM LOGIC
+    fsm_ctrl_stat : process(current_state_stat, crack_count, select_status, status_clear)
+	begin
+        -- Defaults
+        next_state_stat <= current_state_stat;
+        quadcore_count_load <= '0';
+        quadcore_count_in <= 0;
+        quadcore_count_en <= '0';
+        status_rts <= '0';
+        status_ready <= '0';
+
+        case current_state_stat is
+            when S_RESET =>
+                next_state_stat <= RESTART;
+            when RESTART =>
+                -- RESTART COUNTER
+                quadcore_count_load <= '1';
+                quadcore_count_in <= 0;
+                next_state_stat <= FILL_BUFFER;
+            when FILL_BUFFER =>
+                -- ENABLE COUNTER TO FILL THE BUFFER
+                quadcore_count_en <= '1';
+                if crack_count = NUMBER_OF_QUADCORES-1 then
+                    next_state_stat <= READY_TO_SEND;
+                end if;
+            when READY_TO_SEND =>
+                status_rts <= '1';
+                if select_status = '1' then
+                    next_state_stat <= WAIT_FOR_CLEAR;
+                end if;
+            when WAIT_FOR_CLEAR =>
+                status_rts <= '1';
+                status_ready <= '1';
+                -- INIT COUNTERS FOR DATA OUT
+                data_0_count_load <= '1';
+                data_0_count_in <= 0;
+                data_1_count_load <= '1';
+                data_1_count_in <= 0;
+                -- IF TX PIPELINE INTERFACE IS READY
+                if status_clear = '1' then
+                    next_state_stat <= SEND_DATA;
+                end if;
+            when SEND_DATA =>
+                status_rts <= '1';
+                -- ENABLE 8 BIT INDEX COUNTER OF 32 BITS DATA
+                data_0_count_en <= '1';
+                -- IF ALL 32 BITS HAVE BEEN TRANSMITTED
+                if data_0_count = 3 then
+                    -- ENABLE BUFFER INDEX COUNTER
+                    data_1_count_en <= '1';
+                    -- RESET 8 BIT INDEX COUNTER
+                    data_0_count_load <= '1';
+                    data_0_count_in <= 0;
+                end if;
+                -- IF ALL BUFFER HAVE BEEN TRANSMITTED
+                if data_1_count = NUMBER_OF_QUADCORES-1 then
+                    next_state_stat <= RESTART;
+                end if;
+        end case;
+    end process;
+
+    -- FSM PASSWORD
+    -- FSM UPDATE
+    fsm_state_pwd : process(clk)
+    begin
+        if rising_edge(clk) then
+            if reset = '1' then
+                current_state_pwd <= S_RESET;
+            else
+                current_state_pwd <= next_state_pwd;
+            end if; -- rst
+        end if; -- clk
+    end process;
+
+    -- FSM LOGIC
+    fsm_ctrl_pwd : process(current_state_pwd)
+	begin
+        -- Defaults
+        next_state_pwd <= current_state_pwd;
+
+        case current_state_pwd is
+            when S_RESET =>
+                next_state_pwd <= WAIT_FOR_SUCCESS;
+            when WAIT_FOR_SUCCESS =>
+            when GET_PASSWORD =>
+            when READY_TO_SEND =>
+            when WAIT_FOR_CLEAR =>
+            when SEND_DATA =>
+        end case;
+    end process;
+
+    -- SELECT WHETHER OUTPUT STATUS OR PASSWORD
+    select_status <= '1'; -- FOR NOW ONLY OUTPUT STATUS
+
+    -- OUTPUT DATA
+    data <= crack_count_buffer(data_1_count)((4-data_0_count * 8) - 1 downto (3-data_0_count * 8));
 end architecture;
